ef6d6f7f4d96dbd39341e3afbd88c0c49688e9de
Hoon Lee <yl.hoony@gmail.com>
2018-10-22 16:58:52 -0700

    set logic for routes

--- app/controllers/api/v1/countries_controller.rb
+++ app/controllers/api/v1/countries_controller.rb
@@ -1,2 +1,41 @@
+# frozen_string_literal: true
+
 class Api::V1::CountriesController < ApplicationController
+  before_action :set_country, only: %i[show update]
+
+  def index
+    countries = Country.all.to_a
+    render json: countries
+  end
+
+  def create
+    country = Country.new(country_params)
+    if country.save
+      render json: country
+    else
+      render json: { error: country.errors.messages }
+    end
+  end
+
+  def show
+    render json: country if @country.valid?
+  end
+
+  def update
+    if @country.update(country_params)
+      render json: @country
+    else
+      render json: { error: @country.errors.messages }
+    end
+  end
+
+  protected
+
+  def set_country
+    @country = Country.find(params[:id])
+  end
+
+  def country_params
+    params.require(:country).permit(:name, :iso_2, :iso_3, :active)
+  end
 end

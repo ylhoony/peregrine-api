7d70f254e8934715b7fca441791ffe63a1ee09c7
Hoon Lee <yl.hoony@gmail.com>
2018-10-22 16:56:28 -0700

    added validation for countries

--- app/models/country.rb
+++ app/models/country.rb
@@ -4,5 +4,9 @@ class Country
   field :name, type: String
   field :iso_2, type: String
   field :iso_3, type: String
-  field :active, type: Boolean
+  field :active, type: Boolean, default: true
+
+  validates :name, presence: true, uniqueness: true
+  validates :iso_2, presence: true, uniqueness: true
+  validates :iso_3, presence: true, uniqueness: true
 end

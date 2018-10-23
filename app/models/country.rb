afa3286d318f18efdc7ddd01c5faa5791c7a01ed
Hoon Lee <yl.hoony@gmail.com>
2018-10-22 17:03:01 -0700

    added numeric

--- app/models/country.rb
+++ app/models/country.rb
@@ -4,9 +4,11 @@ class Country
   field :name, type: String
   field :iso_2, type: String
   field :iso_3, type: String
+  field :numeric, type: String
   field :active, type: Boolean, default: true
 
   validates :name, presence: true, uniqueness: true
   validates :iso_2, presence: true, uniqueness: true
   validates :iso_3, presence: true, uniqueness: true
+  validates :numeric, presence: true, uniqueness: true
 end

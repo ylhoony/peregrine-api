1c98d285149a7e894ef26b0ba5757e2dbd448d5a
Hoon Lee <yl.hoony@gmail.com>
2018-10-22 17:06:56 -0700

    generated model, and set field info

---
+++ app/models/currency.rb
@@ -0,0 +1,12 @@
+class Currency
+  include Mongoid::Document
+
+  field :name, type: String
+  field :iso_3, type: String
+  field :numeric, type: String
+  field :active, type: Boolean
+
+  validates :name, presence: true, uniqueness: true
+  validates :iso_3, presence: true, uniqueness: true
+  validates :numeric, presence: true, uniqueness: true
+end

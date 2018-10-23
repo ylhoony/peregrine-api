962c7c13baf562b727fd1dcde879516c612d36c1
Hoon Lee <yl.hoony@gmail.com>
2018-10-22 17:19:16 -0700

    copied over the logic from countries controller, and adjusted

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::CurrenciesController < ApplicationController
+  before_action :set_currency, only: %i[show update]
+
+  def index
+    countries = Currency.all.to_a
+    render json: countries
+  end
+
+  def create
+    currency = Currency.new(currency_params)
+    if currency.save
+      render json: currency
+    else
+      render json: { error: currency.errors.messages }
+    end
+  end
+
+  def show
+    render json: currency if @currency.valid?
+  end
+
+  def update
+    if @currency.update(currency_params)
+      render json: @currency
+    else
+      render json: { error: @currency.errors.messages }
+    end
+  end
+
+  protected
+
+  def set_currency
+    @currency = currency.find(params[:id])
+  end
+
+  def currency_params
+    params.require(:currency).permit(:name, :iso_3, :numeric, :active)
+  end
 end

554c236c7490dfc7e4b6b2eebcc654017ac33089
Hoon Lee <yl.hoony@gmail.com>
2018-11-06 17:23:45 -0800

    added devise-jwt module

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
 
   ## Database authenticatable
   field :email,              type: String, default: ""

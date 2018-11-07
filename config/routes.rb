71fdde395c13d29dfc3826e07d00fddfcabe5991
Hoon Lee <yl.hoony@gmail.com>
2018-11-06 17:22:50 -0800

    set up users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,27 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  
-  namespace :api do
-    namespace :v1 do
-      resources :countries, only: [:index, :create, :show, :update]
-      resources :currencies, only: [:index, :create, :show, :update]
-    end
+  scope :api, defaults: {format: :json} do
+    devise_for :users,
+      path: "v1",
+      path_names: {
+        registration: 'sign_up',
+        sign_in: 'sign_in',
+        sign_out: 'sign_out',
+        password: 'secret',
+        confirmation: 'verification',
+        unlock: 'unblock',
+      },
+      controllers: {
+        sessions: 'api/v1/users/sessions',
+        registrations: 'api/v1/users/registrations'
+      }
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :countries, only: %i[index create show update]
+      resources :currencies, only: %i[index create show update]
+    end
+  end
 end

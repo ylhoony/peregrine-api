cb6eafd6805d8bcf8cfb136568919cfd665ef0ad
Hoon Lee <yl.hoony@gmail.com>
2018-11-14 16:18:16 -0800

    created accounts controller and set basic route methods

---
+++ app/controllers/api/v1/accounts_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::AccountsController < Api::V1::ApiController  
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def account_params
+    
+  end
+
+end

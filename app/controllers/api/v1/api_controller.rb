49dc9403bf0a891a8299d231e873ee8f08459797
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 12:12:30 -0800

    set current_user route with authentication func

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
 class Api::V1::ApiController < ApplicationController
+  respond_to :json
+  before_action :authenticate_token!
+  before_action :authenticate_user!
+
+  def current_user
+    render json: { user: @current_user }
+  end
+
+  protected
+
+  def authenticate_token!
+    payload = JwtService.decode(token: auth_token)
+    @current_user = User.find(payload[0]["sub"])
+  rescue JWT::ExpiredSignature
+    render json: { errors: ["Auth token has expired"] }, status: :unauthorized
+  rescue JWT::DecodeError
+    render json: { errors: ["Invalid auth token"] }, status: :unauthorized
+  end
+
+  def auth_token
+    @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
+  end
 end

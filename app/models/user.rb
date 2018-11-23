9004a4bcbe58e100d873cc6470a556c905180a26
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 12:11:04 -0800

    removed devise-jwt set-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
+  has_many :employees
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
+         :recoverable, :rememberable, :validatable
 
   ## Database authenticatable
   field :email,              type: String, default: ""

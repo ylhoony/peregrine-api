bc84c5a8e4dccfa7c2086b34843f38e6e4b93f89
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 12:10:32 -0800

    set fields and relation

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,14 @@
 class Account
   include Mongoid::Document
-  include Mongoid::Timestampss
+  include Mongoid::Timestamps
+
+  belongs_to :country
+  has_many :employees
+
+  field :company_name, type: String
+  field :contact, type: String
+  field :address_1, type: String
+  field :address_1, type: String
+  field :city, type: String
+  field :state, type: String
 end

17c45f62f878c8f2b49e7403645f11a61dcdee8f
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 11:16:57 -0800

    created and added countries and currencie

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+countries = Country.create([
+  { name: "United States", iso_2: "US", iso_3: "USA", numeric: "840" },
+  { name: "Canada", iso_2: "CA", iso_3: "CAN", numeric: "124" }
+])
+
+currnecies = Currency.create([
+  { name: "Canadian Dollar", iso_3: "CAD", numeric: "124" },
+  { name: "US Dollar", iso_3: "USD", numeric: "840" }
+])

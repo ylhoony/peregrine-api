0e2cb9ddc5d09a6bec7c64672ecb96af34bf9ef6
Hoon Lee <yl.hoony@gmail.com>
2018-11-23 11:28:31 -0800

    customized to use custom JWTService

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json
 
   # GET /resource/sign_in
   # def new
@@ -12,6 +13,34 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   # def create
   #   super
   # end
+
+  def create
+    self.resource = User.find_by(email: sign_in_params[:email])
+
+    if resource.persisted?
+      if resource.valid_password?(sign_in_params[:password])
+        sign_in(resource_name, resource) # Update sign in record with Devise method.
+
+        payload = {
+          sub: resource.id.to_str,
+          iat: Time.now.to_i,
+          exp: Time.now.to_i + 24 * 60 * 60,
+        }
+
+        token = JwtService.encode(payload: payload)
+
+        response['Authorization'] = token
+        render json: { current_user: resource }
+      else
+        # binding.pry
+        render json: { errors: "Incorrect username or password." }
+      end
+    else
+      # binding.pry
+      render json: { errors: resource.errors.messages }
+    end
+
+  end
 
   # DELETE /resource/sign_out
   # def destroy
